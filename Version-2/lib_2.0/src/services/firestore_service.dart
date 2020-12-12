3d896033930ab22d010528b3a0fc46035b64ca30
AkshaySB2000 <53827046+AkshaySB2000@users.noreply.github.com>
2020-12-12 12:33:48 +0530

    Add files via upload

---
+++ Version-2/lib_2.0/src/services/firestore_service.dart
@@ -0,0 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:farmers_market/src/models/product.dart';
+import 'package:farmers_market/src/models/user.dart';
+
+class FirestoreService {
+  FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  Future<void> addUser(MyUser user) {
+    return _db.collection('users').doc(user.userId).set(user.toMap());
+  }
+
+  Future<MyUser> fetchUser(String userId) {
+    return _db
+        .collection('users')
+        .doc(userId)
+        .get()
+        .then((snapshot) => MyUser.fromFirestore((snapshot.data())));
+  }
+
+  Stream<List<String>> fetchUnitTypes() {
+    return _db.collection('types').doc('units').snapshots()
+          .map((snapshot) => snapshot.data()['production']
+          .map<String>((type) => type.toString()).toList());      
+  } 
+
+  Future<void> setProduct(Product product) {
+    return _db.
+    collection('products')
+    .doc(product.productId)
+    .set(product.toMap());
+  }
+
+  Future<Product> fetchProduct(String productId) {
+    return _db.collection('products').doc(productId)
+    .get().then((snapshot) => Product.fromFirestore(snapshot.data()));
+  }
+
+  Stream<List<Product>> fetchProductByVendorId(String vendorId) {
+    return _db
+           .collection('products')
+           .where('vendorId', isEqualTo: vendorId)
+           .snapshots()
+           .map((query) => query.docs)
+           .map((snapshot) => 
+            snapshot.map((document) => Product.fromFirestore(document.data()))
+           .toList());
+  }
+}
